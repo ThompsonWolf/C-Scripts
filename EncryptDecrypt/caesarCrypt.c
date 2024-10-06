9ab1c479b07d3e1489be81a8aac5d68e89a53d65
Thompson Wolf <lonedevwolf@gmail.com>
2024-10-07 00:39:00 +0200

    An EncryptDecrypt file Caeser Cipher

---
+++ EncryptDecrypt/caesarCrypt.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+void encrypt(char *message, int key) {
+    for (int i = 0; message[i] != '\0'; ++i) {
+        char ch = message[i];
+        if (ch >= 'a' && ch <= 'z') {
+            ch = ch + key;
+            if (ch > 'z') {
+                ch = ch - 'z' + 'a' - 1;
+            }
+            message[i] = ch;
+        } else if (ch >= 'A' && ch <= 'Z') {
+            ch = ch + key;
+            if (ch > 'Z') {
+                ch = ch - 'Z' + 'A' - 1;
+            }
+            message[i] = ch;
+        }
+    }
+}
+
+void decrypt(char *message, int key) {
+    for (int i = 0; message[i] != '\0'; ++i) {
+        char ch = message[i];
+        if (ch >= 'a' && ch <= 'z') {
+            ch = ch - key;
+            if (ch < 'a') {
+                ch = ch + 'z' - 'a' + 1;
+            }
+            message[i] = ch;
+        } else if (ch >= 'A' && ch <= 'Z') {
+            ch = ch - key;
+            if (ch < 'A') {
+                ch = ch + 'Z' - 'A' + 1;
+            }
+            message[i] = ch;
+        }
+    }
+}
+
+int main() {
+    char message[100];
+    int key;
+
+    printf("Enter a message to encrypt: ");
+    scanf("%s", message);
+    printf("Enter key: ");
+    scanf("%d", &key);
+
+    encrypt(message, key);
+    printf("Encrypted message: %s\n", message);
+
+    decrypt(message, key);
+    printf("Decrypted message: %s\n", message);
+
+    return 0;
+}
